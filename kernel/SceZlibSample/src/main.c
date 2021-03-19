84d830ae153d1535b04b97ecfd398f2c8db6b48c
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-03-19 18:15:38 +0900

    Add SceZlib file sample

--- kernel/SceZlibSample/src/main.c
+++ kernel/SceZlibSample/src/main.c
@@ -185,10 +185,140 @@ int zlib_test(void){
 	return 0;
 }
 
+int zlib_test_file(void){
+
+	int res;
+
+	GetExport("SceSblPostSsMgr", 0xE241534E, 0x25F28DA7, &sceZlibDeflateInit);
+	GetExport("SceSblPostSsMgr", 0xE241534E, 0x20A122F8, &SceZlibForDriver_20A122F8);
+	GetExport("SceSblPostSsMgr", 0xE241534E, 0x5492B3F2, &sceZlibDeflateEnd);
+	GetExport("SceSblPostSsMgr", 0xE241534E, 0x5B718E55, &sceZlibDeflate);
+
+	SceKernelHeapCreateOpt heap_opt;
+	memset(&heap_opt, 0, sizeof(heap_opt));
+	heap_opt.size     = sizeof(heap_opt);
+	heap_opt.uselock  = SCE_KERNEL_HEAP_ATTR_HAS_AUTO_EXTEND | SCE_KERNEL_HEAP_ATTR_HAS_MEMORY_TYPE;
+	heap_opt.field_10 = 0x10F0D006;
+
+	heapid = ksceKernelCreateHeap("SceZlibSampleHeap", 0x2000, &heap_opt);
+	if(heapid < 0){
+		ksceDebugPrintf("sceKernelCreateHeap failed : 0x%X\n", heapid);
+		return -1;
+	}
+
+	SceUID memid = ksceKernelAllocMemBlock("SceZlibMemblk", 0x10F0D006, 0x800000, NULL);
+	if(memid < 0){
+		ksceDebugPrintf("sceKernelAllocMemBlock failed : 0x%X\n", memid);
+		goto del_heap;
+	}
+
+	void *zlib_output = NULL;
+	ksceKernelGetMemBlockBase(memid, &zlib_output);
+
+	memset(zlib_output, 0, 0x800000);
+
+	SceZlibStream strm;
+	memset(&strm, 0, sizeof(strm));
+
+	strm.zalloc = my_zlib_alloc;
+	strm.zfree  = my_zlib_free;
+	strm.opaque = zlib_args;
+
+#if SCE_ZLIB_OUTPUT_GZIP == 0
+	res = sceZlibDeflateInit(&strm, 1, "1.2.5", sizeof(strm));
+	ksceDebugPrintf("sceZlibDeflateInit:0x%X\n", res);
+#else
+	res = SceZlibForDriver_20A122F8(&strm, 1, 8, 0x18, 1, 0, "1.2.5", sizeof(strm));
+	ksceDebugPrintf("SceZlibForDriver_20A122F8:0x%X\n", res);
+#endif
+
+	strm.next_in  = NULL;
+	strm.avail_in = 0;
+	strm.total_in = 0;
+	strm.next_out  = zlib_output;
+	strm.avail_out = 0x800000;
+	strm.total_out = 0;
+
+	int file_size, zlib_res = -1;
+
+	SceUID fd = ksceIoOpen("sd0:os0.bin", SCE_O_RDONLY, 0);
+	if(fd < 0){
+		ksceDebugPrintf("sceIoOpen failed : 0x%X\n", fd);
+		goto del_memblk;
+	}
+
+	SceIoStat stat;
+
+	res = ksceIoGetstatByFd(fd, &stat);
+	if(res < 0){
+		ksceDebugPrintf("sceIoGetstatByFd failed : 0x%X\n", res);
+		goto io_close;
+	}
+
+	file_size = (int)stat.st_size;
+
+	do {
+		memset(working_buffer, 0, sizeof(working_buffer));
+		res = ksceIoRead(fd, working_buffer, sizeof(working_buffer));
+		if(res > 0){
+			file_size    -= res;
+			strm.next_in  = working_buffer;
+			strm.avail_in = res;
+
+			zlib_res = sceZlibDeflate(&strm, (file_size != 0) ? 0 : 4);
+		}
+	} while(res > 0 && zlib_res == 0);
+
+io_close:
+	ksceIoClose(fd);
+
+	if(zlib_res == 1){
+		char drv[8], ext[8], path[0x40];
+
+#if SCE_ZLIB_OUTPUT_GZIP == 0
+		snprintf(ext, sizeof(ext), "bin");
+#else
+		snprintf(ext, sizeof(ext), "gzip");
+#endif
+
+		SceIoStat stat;
+		if(ksceIoGetstat("host0:", &stat) >= 0){
+			snprintf(drv, sizeof(drv), "host0:");
+		}else if(ksceIoGetstat("uma0:", &stat) >= 0){
+			snprintf(drv, sizeof(drv), "uma0:");
+		}else if(ksceIoGetstat("sd0:", &stat) >= 0){
+			snprintf(drv, sizeof(drv), "sd0:");
+		}else if(ksceIoGetstat("ux0:", &stat) >= 0){
+			snprintf(drv, sizeof(drv), "ux0:");
+		}else{
+			drv[0] = 0;
+		}
+
+		if(drv[0] != 0){
+			snprintf(path, sizeof(path), "%s/zlib_sample_output.%s", drv, ext);
+			write_file(path, zlib_output, strm.total_out);
+		}
+	}
+
+	res = sceZlibDeflateEnd(&strm);
+	ksceDebugPrintf("sceZlibDeflateEnd:0x%X\n", res);
+
+del_memblk:
+	ksceKernelFreeMemBlock(memid);
+	memid = -1;
+
+del_heap:
+	ksceKernelDeleteHeap(heapid);
+	heapid = -1;
+
+	return 0;
+}
+
 void _start() __attribute__ ((weak, alias("module_start")));
 int module_start(SceSize args, void *argp){
 
-	zlib_test();
+	// zlib_test();
+	zlib_test_file();
 
 	return SCE_KERNEL_START_NO_RESIDENT;
 }
